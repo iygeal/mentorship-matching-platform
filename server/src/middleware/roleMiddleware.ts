8323ab7017aac810ea80d21e6277cc03efdd4a6e
Innocent Anozie <iygeal@gmail.com>
2025-07-02 09:30:04 +0100

    NEW: Added middleware to restrict some actions from other users who are not admins

---
+++ server/src/middleware/roleMiddleware.ts
@@ -0,0 +1,13 @@
+import { Response, NextFunction } from "express";
+import { AuthenticatedRequest } from "./authMiddleware";
+
+export const isAdmin = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.user || req.user.role !== "admin") {
+    return res.status(403).json({ message: "Access denied. Admins only." });
+  }
+  next();
+};
