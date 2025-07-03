71b8d33055449c3088e970c96eacd5be7b1ebc84
Innocent Anozie <iygeal@gmail.com>
2025-07-04 00:10:18 +0100

    Scafolded frontend with Vite and ReactJS

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
