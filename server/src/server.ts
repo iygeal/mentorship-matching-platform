36fa6fe17e8209eea0534a7890ccb70cfe89401b
Innocent Anozie <iygeal@gmail.com>
2025-07-05 21:48:32 +0100

    UPDATE: Installed and added CORS to the backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import mainRoutes from "./routes";
 import connectDB from "./config/db";
 
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1", mainRoutes);
 
