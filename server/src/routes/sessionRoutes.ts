4e224e0c51a53e8b620c6ba085a592408c928935
Innocent Anozie <iygeal@gmail.com>
2025-07-08 09:55:43 +0100

    Added a new getMySessions controller and route to the backend

--- server/src/routes/sessionRoutes.ts
+++ server/src/routes/sessionRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { bookSession, addFeedback } from "../controllers/sessionController";
+import { bookSession, addFeedback, getMySessions } from "../controllers/sessionController";
 import protect from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.post("/", protect, bookSession);
 router.put("/:id/feedback", protect, addFeedback);
+router.get("/", protect, getMySessions);
 
 export default router;
